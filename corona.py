2a0b4e50495d01f701ad4ef5f3fd723dc219e9c8
Guli <ignaciomorenopubul@gmail.com>
2020-03-13 01:48:19 +0100

    Change method

--- corona.py
+++ corona.py
@@ -12,21 +12,28 @@ st.write(
 	# COVID-19: The mathematical reason to wash your hands :sweat_drops::clap:
 	_Over the past few days, Coronavirus has taken the world by storm. As governments rush to take action to minimize the spread of the disease, we – as citizens – also have to stop and think about the individual contributions we can have to #flattenthecurve.
 	We'll be looking at how staying at home and washing our hands frequently can help us beat the disease – using math!_🤓 
-	## The SIR Method
-	By **moving the slider below**, you can play around with the *Contact rate*, 𝛽 of the virus so see how it would affect the evolution of the disease! You'll find the full explanation below the chart.
+	## Let's get started
+	The [Basic Reproduction Number](https://en.wikipedia.org/wiki/Basic_reproduction_number),$\ R_0$ of an infection is how many people of a population a typical case might infect. So, an$\ R_0 = 2$ means that a typical case infects 2 more people.
+	By **moving the slider below**, you can play around with the$\ R_0$ of COVID-19 and see how changing it would affect the evolution of the disease! You'll find the full explanation below the chart.
 	'''
 )
-betaInt = st.slider('Contact rate, 𝛽', 0.2, 0.4, 0.4, 0.05)
 
 ######################## Current Scenario ########################
 # Total population, N.
 N = 6662000
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 =  1, 0
+I0, R0 =  2, 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
-# Contact reasoningte, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.55, 1./14 
+# Contact and recovery times
+R0 = 6.49
+Tr = 14
+Tc = Tr/R0
+
+R0Int = st.slider('Average number of people infected by each sick person', 0.5, 6.49, 4.00, 0.05)
+betaInt = R0Int/Tr
+# Contact reasoning rate, beta, and mean recovery rate, gamma, (in 1/days).
+beta, gamma = 1/Tc, 1./Tr
 # A grid of time points (in days)
 t = np.linspace(0, 365, 365)
 
@@ -72,7 +79,7 @@ intTrace = go.Scatter(
 currentTrace = go.Scatter(
 	x=datelist, 
 	y=I,
-	name="Scenario with no safety measures (𝛽 = 0.5)",
+	name="Scenario with no safety measures",
 	line=dict(color='Red'),
 	fill='tozeroy',
 	hovertemplate =
@@ -108,30 +115,41 @@ def human_format(num):
 deltaInfections = human_format(np.amax(I) - np.amax(IInt))
 deltaDays = np.argmax(IInt) - np.argmax(I)
 st.write(
-	'> ### A 𝛽 of', betaInt, 'renders', deltaInfections, 'less infections and the peak is delayed by ', deltaDays, 'days with respect to the scenario with no safety measures.'
+	'> ### An$\ R_0$ of', R0Int, 'renders', deltaInfections, 'less infections and the peak is delayed by ', deltaDays, 'days with respect to the scenario with no safety measures.'
 )
 st.write(
 	'''
 	### :mag_right: Conclusions
-	The parameter 𝛽 represents how often contacts with infected population end in infection. This is the only parameter we – as a society – can change in this mathematical model. We *need* to push this value lower. Why?
+	$\ R_0$ is a good predictor of how an infection might evolve. This is the only parameter we – as a society – can change in this mathematical model. We *need* to push this value lower. Why?
 	The chart above gives us some clues!
 	* The **lower** this value, the **lower** the **peak number** of infected individuals.
 	* The **lower** this value, the **later** the peak of infected individuals. 
 	
-	In other words, a lower 𝛽 gives us **less total impact** and **more time** to prepare and care for the infected. So, how do we do this?
-	* :house_with_garden: We try to stay at home as much as possible. If we all do this, we increase the time between contacts (lowering 𝛽).
-	* :sweat_drops::clap: We keep an eye on our hygiene (a.k.a. properly wash our hands), avoid touching our face, and try to keep at least 1.5 meters of distance with other people. That way if contacts do ocurr, the probability of contagion is as low as possible.
+	In other words, a lower$\ R_0$ gives us **less total impact** and **more time** to prepare and care for the infected. If we can push$\ R_0$ below **1**, we're in luck! If **each sick person** infects on average **less than one** person, the disease **dies out**.  So, how do we do this?
+	* **Social Distancing :house_with_garden::** We try to stay at home as much as possible. If we all do this, we increase the time between contacts with potential sources of virus. Therefore the chances of infecting people or becoming infected ourselves diminishes and$\ R_0$ increases!
+	* **Hygiene :sweat_drops::clap:: **We keep an eye on our hygiene (i.e. properly wash our hands), avoid touching our face, and try to keep at least 1.5 meters of distance with other people. That way if contacts with a source of virus do ocurr, the probability of contagion is as low as possible, and$\ R_0$ again increases!
 
-	**Play around with the slider above to lower 𝛽** and see this concept in action!
+	**Play around with the slider above to lower$\ R_0$** and see this concept in action!
 	'''
+)
+st.write(
+	'> ### :warning: The sooner you start taking measures, the better! Start today!'
 )
 st.write(
 	'''
 	### :thinking_face: So, what's going on?
-	With the [SIR epidemic model] (https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SIR_model_without_vital_dynamics) is governed by 2 parameters: 
-	* **The Contact rate, 𝛽:** This is the **key** parameter in this exercise. It represents the typical time between contacts. It's actually the inverse of that, so a *𝛽 = 0.5* means 2 days between contacts.
+	This [SIR epidemic model] (https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SIR_model_without_vital_dynamics) on which I have based this experiment is governed by 2 parameters: 
 	* **The Recovery rate, $\gamma$: ** This is the inverse of the time an infected individual is contagious. For this exercise, I've used 14 days, which is the recommended quarantine period.
+	* **The Contact rate, 𝛽:** This is the **key** parameter in this exercise. It represents the typical time between contacts. It's actually the inverse of that, so a *𝛽 = 0.5* means 2 days between contacts. Since we can't really affect $\gamma$, this is the parameter we are actually modifying with our safety measures.
 
+	They happen to be the parameters that make up$\ R_0$:
+	'''
+)
+st.latex(r'''
+	R_0 = \frac{\beta}{\gamma},
+''')
+st.write (
+	'''
 	The SIR model describes the evolution of COVID-19 as a set of ordinary differential equations:
 	'''
 )
@@ -148,13 +166,19 @@ st.write(
 	'Where$\ S$ is the stock of susceptible population,$\ I$ is the stock of infected, and$\ R$ is the stock of recovered population.'
 )
 st.write(
-	'''This exercise is modelled after the **Community of Madrid** case. So, I have used a population$\ N = 6.66M$ people, and initial conditions$\ I_0 = 1$ infected and $\ R_0 = 0$ recovered.
-	Then, I used [historical data](https://www.epdata.es/casos-coronavirus-comunidad-madrid/090fa67e-2256-43bd-a7b8-fdfc443da658) of the evolution of the virus to fit a 𝛽 for the *Worst Case Scenario* of '''
-	, 0.5, 'which seemed to adjust quite well to the growth numbers of the first 15 days in Madrid'
+	'''This exercise is modelled after the **Community of Madrid** case. So, I have used a population$\ N = 6.66M$ people, and initial conditions$\ I_{init} = 2$ infected and $\ R_{init} = 0$ recovered.
+	Then, I used [historical data](https://www.epdata.es/casos-coronavirus-comunidad-madrid/090fa67e-2256-43bd-a7b8-fdfc443da658) of the evolution of the virus to fit a$\ R_0$ for the *Scenario with no safety measures* of '''
+	, 6.49, 'which seemed to adjust quite well to the growth numbers of the first 15 days in Madrid, and happens to coincide with the upper limits of [some studies](https://www.medicalnewstoday.com/articles/coronavirus-may-spread-faster-than-who-estimate#Higher-estimates-than-WHO-predict)'
 )
 st.write(
 	'''>:warning: *Note: This exercise was performed on **12/03/2020**.
 	By the time you read this, it is likely the fit no longer applies. Also, I'm not an epidemologist, so take it all with a grain of salt and don't forget to follow your local government health and safety regulations!*'''
+)
+st.write(
+'''
+------
+Made with :heart: in Madrid by [Gulipad](https://www.linkedin.com/in/imorenopubul/)
+'''
 )
 
 
